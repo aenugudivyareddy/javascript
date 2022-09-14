77020a649f6088ac5c46d2f2e05c76b52410e66f
aenugudivyareddy <divya@digitaldots.ai>
2022-09-14 19:33:22 +0530

    functions loops

--- app.js
+++ app.js
@@ -1,1 +1,119 @@
-console.log("Hello from our first js file")
+//console.log("Hello from our first js file")
+
+
+//for(let i=1;i<=10;i++){
+   // console.log(i)
+//}
+
+
+//for (let i=0; i<=20;i++){
+  //  console.log(i)
+//}
+
+// for (let i=0;i<=20;i+=2){
+//     console.log(i)
+// }
+
+
+// for (let i=100;i>=0;i-=10){
+//     console.log(i)
+// }
+
+// for (let i=10;i<=1000;i*10){
+//     console.log(i)
+// }
+
+// for (let i=25;i>=0;i-=5){
+//     console.log(i)
+// }
+
+// const animals=['tiger','lion','deer'];
+// for (let i=0;i<animals.length;i++){
+//     console.log(i,animals[i])
+// }
+
+// const animals=['tiger','lion','deer'];
+// for(let i=animals.length-1;i>=0;i--){
+//     console.log(i,animals[i])
+// }
+
+// const people=['ramu','venu','geetha','neetha']
+// for(let i=0;i<people.length;i++){
+//     console.log(i,people[i])
+// }
+
+// for(let i=1;i<=10;i++){
+//     console.log(`i is: ${i}`)
+//     for(let j=1;j<4;j++){
+//         console.log(`   j is:${j}`)
+//     }
+// }
+
+
+// let num=0;
+// while(num<10){
+//     num++;
+//     console.log(num);
+    
+// }
+
+
+// const password="divyareddy";
+// let guess=prompt("enter the password");
+// while(guess=pasword){
+//     guess=prompt("enter the pssword");
+
+// }
+// console.log("you won")
+
+
+// for (let i=0;i<=1000;i++){
+//     console.log(i);
+//     if (i===100) break;
+// }
+
+
+// for(let char of "helloworld"){
+//     console.log(char)
+// }
+
+
+// function singSong(){
+//     console.log("hi");
+//     console.log("am");
+//     console.log("here");
+// }
+
+// function repeat(str,numTimes){
+//     let result='';
+//     for (let i=0;i<numTimes;i++){
+//         result+=str;
+
+//     }
+//     console.log(result);
+// }
+
+// function add(x,y){
+//     console.log(x+y)
+// }
+
+// function add(x,y){
+//     let sum=x+y;
+//     return sum;
+// }
+
+// let radius=8;
+// if(radius>0){
+//     const pi=3.14519;
+//     let msg='hi';
+
+// }
+// console.log(radius);
+
+
+let animal='tiger';
+function animalwatch(){
+    let animal="lion";
+    
+}
+console.log("animal");
